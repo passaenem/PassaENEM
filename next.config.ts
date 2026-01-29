cfaef37fc5bc2ac38aefd46fa164190ca4f40133
Leandro Pires <leandropiresnunes64@gmail.com>
2026-01-29 11:37:14 -0300

    Update

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  devIndicators: {
-    appIsrStatus: false,
-    buildActivity: false,
-    buildActivityPosition: 'bottom-right',
-  },
+
 };
 
 export default nextConfig;
